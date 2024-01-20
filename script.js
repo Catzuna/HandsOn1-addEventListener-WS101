c64e2a4cab02dd4473de4e183c2b6ed20abbe5a5
Josu <148201811+Catzuna@users.noreply.github.com>
2024-01-20 23:17:16 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+function handleClickBtn1() {
+    alert("Button 1 was clicked!");
+}
+
+function handleClickBtn2() {
+    alert("Button 2 was clicked!");
+}
+
+function handleClickBtn3() {
+    alert("Button 3 was clicked!");
+}
+
+document.getElementById("btn1").addEventListener("click", handleClickBtn1);
+document.getElementById("btn2").addEventListener("click", handleClickBtn2);
+document.getElementById("btn3").addEventListener("click", handleClickBtn3);
